38608eb65a770ae5c261b0dffb0c56dfcbd548cb
mivillan <awkwardmoog@gmail.com>
2024-04-04 14:43:30 -0500

    added window

--- src/CreateController.py
+++ src/CreateController.py
@@ -1,6 +1,15 @@
 print("hello!!")
 import maya.cmds as mc
 
+from PySide2.QtWidgets import QWidget  #all outlines, window, graph editors are widgets
+
+class CreateControllerWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Create IKFK Limb")
+
+controllerWidget = CreateControllerWidget()
+controllerWidget.show()
 
 
 
