9ac3e62371512802327943dc440c837586fd2c07
mivillan <awkwardmoog@gmail.com>
2024-04-04 14:24:44 -0500

    initial commit

---
+++ src/CreateController.py
@@ -0,0 +1,18 @@
+print("hello!!")
+import maya.cmds as mc
+
+
+
+
+
+
+
+
+
+
+
+
+
+#send to maya through Alt+Shift+M, will run through MEL
+# use "commandPort -name "localhost:7001" -sourceType "mel";
+#to open file, go to search bar and type "CMD". Once prompted, type "code ." into the bar for it to run visual studio. 
